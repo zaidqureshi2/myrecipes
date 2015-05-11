b0fa3a74df2fc68c7a016d9a777bb0419766305f
Zaid Qureshi <zaid.qureshi@hotmail.ca>
2015-05-11 02:31:25 +0000

    Create new and edit form and add partials

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def gravatar_for(chef, options={size:80 })
+    gravator_id = Digest::MD5::hexdigest(chef.email.downcase)
+    size = options[:size]
+    gravator_url="https://secure.gravatar.com/avatar/#{gravator_id}?s=#{size}"
+    image_tag(gravator_url, alt: chef.name, class: "gravatar")
+  end
 end
